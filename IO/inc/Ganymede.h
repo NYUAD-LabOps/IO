bcdcad897d292f41ba5e421b689af782e57f146b
cdavis0 <cdavis0>
2020-10-11 17:11:08 -0500

    Oct 11 20

--- IO/inc/Ganymede.h
+++ IO/inc/Ganymede.h
@@ -1,6 +1,12 @@
 #include <Ethernet.h>
 #include <USB.h>
 
+#define     MAX_NUM_OF_TRY              (1000000)
+#define     UX_STORAGE_BUFFER_SIZE      (64*1024)
+
+#define     EVENT_USB_PLUG_IN           (1UL << 0)
+#define     EVENT_USB_PLUG_OUT          (1UL << 1)
+
 UINT EthernetUDPInit(NX_IP *ip, NX_UDP_SOCKET *udp_sck, UINT port, VOID(*udp_receive_notify)(NX_UDP_SOCKET *socket_ptr));
 
 void EthernetPrimarySend(char *data, unsigned int length, NX_UDP_SOCKET *udp_sck, ULONG ip_address, UINT port,
@@ -10,3 +16,5 @@ UINT EthernetSecondarySend(char *data, unsigned int length, NX_UDP_SOCKET *udp_s
         TX_EVENT_FLAGS_GROUP *udp_echo_received);
 
 UINT USBDeviceRead(UX_SLAVE_CLASS_CDC_ACM *g_cdc, char *rxBuff, UINT length, UINT *actual_length);
+
+UINT usb_host_plug_event_notification(ULONG usb_event, UX_HOST_CLASS *host_class, VOID *instance);
